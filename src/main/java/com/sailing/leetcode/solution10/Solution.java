2e7b5b9422658d89c46acd9095c73783620e8bea
sailingYang <492066199@qq.com>
2018-03-20 23:38:03 +0800

    NFA 不够优化

--- src/main/java/com/sailing/leetcode/solution10/Solution.java
+++ src/main/java/com/sailing/leetcode/solution10/Solution.java
@@ -1,20 +1,19 @@
 package com.sailing.leetcode.solution10;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 /**
  * 2018-03-19 create NFA
  * 2018-03-20 use NFA
+ * beat 8%
  * yangyang
  */
 public class Solution {
-
-
+    private final static int INF = -2;
+    private final static int E = -1;
 
     public boolean isMatch(String s, String p) {
-        if(s.length() == 0 && p.length() == 0){
+        if (s.length() == 0 && p.length() == 0) {
             return true;
         }
         Node head = createNFA(p);
@@ -26,87 +25,49 @@ public class Solution {
         LinkedList<Node> backtrackList = new LinkedList<>();
         int comsume = 0;
         boolean neddBack = false;
-        while(true){
-            System.out.println(comsume);
-            display(head);
-            if(!neddBack) {
-                if(p == null && comsume == s.length()){
-                    return true;
-                }
-                if(p == null){
-                    neddBack = true;
-                    continue;
-                }
-
-                if(p.loop != null){
+        while (true) {
+            if (p == null && comsume == s.length()) {
+                return true;
+            }
+            if (!neddBack && p != null) {
+                if (p.loop != Integer.MIN_VALUE) {
                     backtrackList.add(p);
                     p = p.next;
                     continue;
                 }
 
-                if(p.status == -1 && comsume == s.length()){
-                    return true;
+                if (comsume >= s.length()) {
+                    return false;
                 }
 
-
-                if(comsume >= s.length()){
-                    neddBack = true;
-                    continue;
-                }
-
-                Node next = null;
-                if(p.path.equals(s.charAt(comsume) + "") || p.path.equals("inf")){
+                if (p.path == s.charAt(comsume) || p.path == INF) {
+                    Node next = null;
                     next = p.next;
-                }
-
-                if(next != null){
                     comsume = comsume + 1;
                     p = next;
                 }else {
                     neddBack = true;
                 }
-            }else {
+            } else {
                 //do backtrack
-                boolean canMatch = false;
-                while (!canMatch) {
-                    if(backtrackList.size() == 0){
-                        System.out.println("");
-                    }
-                    p = null;
-                    comsume = 0;
-                    for(Node node : backtrackList){
-                        comsume = comsume + node.useCount;
-                        p = node;
-                    }
-
-                    if(p == null){
+                while (true)  {
+                    p = backtrackList.peekLast();
+                    if (p == null) {
                         return false;
                     }
-
-                    comsume = p.start + comsume;
-                    if(p.status == -1 && comsume == s.length()){
-                        return true;
+                    comsume = p.start;
+                    for (Node node : backtrackList) {
+                        comsume = comsume + node.useCount;
                     }
 
-                    if(comsume >= s.length()){
-
-                        Node t = backtrackList.pollLast();
-                        t.useCount = 0;
-                        continue;
-                    }
-                    char tmp = s.charAt(comsume);
-
-                    if ((tmp + "").equals(p.loop) || p.loop.equals("inf")){
-                        canMatch = true;
-                    }
-
-                    if (!canMatch) {
-                        Node t = backtrackList.pollLast();
-                        t.useCount = 0;
-                    }else {
+                    if (s.charAt(comsume) == p.loop || p.loop == INF) {
                         comsume = comsume + 1;
-                        p.useCount ++;
+                        p.useCount++;
                         p = p.next;
+                        break;
+                    }else {
+                        Node t = backtrackList.pollLast();
+                        t.useCount = 0;
                     }
                 }
                 neddBack = false;
@@ -114,84 +75,51 @@ public class Solution {
         }
     }
 
-    private void display(Node head) {
-        while (head != null){
-            if(head.loop == null){
-                System.out.print("(" + head.path + ")");
-            }else {
-                System.out.print("(" + head.useCount + head.path + ")");
-            }
-            head = head.next;
-        }
-        System.out.println();
-    }
-
 
     private Node createNFA(String p) {
-        Node head = new Node(0);
+        Node head = new Node();
         Node cur = head;
-        int nextStatus = 0;
-        for(int i = 0; i < p.length(); i ++){
+        int oc = 0;
+        for (int i = 0; i < p.length(); i++) {
             char c = p.charAt(i);
-            nextStatus = nextStatus + 1;
-            Node next = new Node(nextStatus);
-            next.pre = cur;
-            int oc = 0;
-            if(i + 1 < p.length() && p.charAt(i + 1) == '*'){
-                if(c == '.'){
-                    cur.loop = "inf";
-                }else {
-                    cur.loop = c + "";
-                }
-                cur.start = oc;
-                if(i + 1 == p.length() - 1){
-                    cur.status = -1;
-                    break;
-                }else {
-                    i = i + 1;
-                    cur.next = next;
-                    cur.path = "^";
-                    cur = next;
-                }
-            }else {
-                if (c == '.') {
-                    cur.next = next;
-                    cur.path = "inf";
-                } else {
-                    cur.next = next;
-                    cur.path = c + "";
-                }
-                oc ++;
-                if (i == p.length() - 1) {
-                    next.status = -1;
-                }
+            cur.start = oc;
+            if (i + 1 < p.length() && p.charAt(i + 1) == '*') {
+                cur.loop = c == '.' ? INF : c;
+                cur.path = E;
+                i = i + 1;
+            } else {
+                cur.path = c == '.' ? INF : c;
+                oc++;
+            }
+
+            if (i < p.length() - 1) {
+                Node next = new Node();
+                cur.next = next;
                 cur = next;
             }
-        }
-        while (cur.pre != null && cur.pre.loop != null){
-            cur.pre.status = -1;
-            cur = cur.pre;
         }
         return head;
     }
 
 
-    static class Node{
-        public Node(int status){
-            this.status = status;
-        }
-        Node pre;
-        String path;
+    static class Node {
+        int path;
         Node next;
-        int status;
 
-        String loop = null;
+        int loop = Integer.MIN_VALUE;
         int useCount;
         int start;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(new Solution().isMatch("aab","a.*b"));
+//        System.out.println(new Solution().isMatch("abaab",
+//                "a*ba*bb*"));
+
+//        System.out.println(new Solution().isMatch("aabccbcbacabaab",
+//                ".*c*a*b.*a*ba*bb*"));
+
+        System.out.println(new Solution().isMatch("ab",
+                ".*c"));
     }
 }
