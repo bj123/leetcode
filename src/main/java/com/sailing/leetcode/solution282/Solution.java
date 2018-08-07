0e4a505a698bb88a0360c21a427e3294a679d820
sailingYang <492066199@qq.com>
2018-08-08 07:08:25 +0800

    xxx

--- src/main/java/com/sailing/leetcode/solution282/Solution.java
+++ src/main/java/com/sailing/leetcode/solution282/Solution.java
@@ -1,5 +1,7 @@
 package com.sailing.leetcode.solution282;
 
+import com.google.common.collect.Sets;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,7 @@ public class Solution {
     }
 
     private List<String> collect(int[] nums, int end, int target) {
+        //
         List<String> rs = new ArrayList<>();
         if(end == 0){
             if(nums[end] == target) {
@@ -41,62 +44,115 @@ public class Solution {
                 rs.add(a + "*" + b);
             }
 
-            if(a * 10 + b == target){
+            if(a * 10 + b == target && a != 0){
                 rs.add(a + "" + b);
             }
             return rs;
         }
 
-        //if last num use +
-        addto(rs, collect(nums, end - 1, target - nums[end]), 1, "+" + nums[end]);
-        //if last num use -
-        addto(rs, collect(nums, end - 1, target + nums[end]), 2, "-" + nums[end]);
-        //if last num use *
-        int p = nums[end];
-        String ppp = String.valueOf(p);
-        for(int i = end; i > 0; i--) {
-            p = p * nums[i - 1];
-            ppp = nums[i - 1] + "*" + ppp;
-            if(i - 2 == -1 ){
-                if(p == target){
-                    rs.add(ppp);
+        List<Info> right = new ArrayList<>();
+        for(int i = end; i >= 0; i--){
+            //* and connect
+            right = construct(i, end, nums, right);
+            for(Info info : right){
+                if(!info.valid){
+                    continue;
                 }
-                break;
-            }
-            addto(rs, collect(nums, i - 2, target - p ), 3, "+" + ppp);
-            addto(rs, collect(nums, i - 2, target + p ), 3, "-" + ppp);
-        }
-        //if use connected
-        p = nums[end];
-        ppp = String.valueOf(p);
-        int base = 10;
-        for(int i = end; i > 0; i--) {
-            p =  p + nums[i - 1] *  base;
-
-            ppp = nums[i - 1] +  ppp;
-            if(i - 2 == -1 ){
-                if(p == target){
-                    rs.add(ppp);
+                if(i == 0 && info.c == target){
+                    rs.add(info.str);
+                    continue;
                 }
-                break;
-            }
-            base = base * 10;
+                //add
+                List<String> add = collect(nums, i - 1, target - info.c);
+                //minus
+                List<String> minus = collect(nums, i - 1, target + info.c);
 
-            addto(rs, collect(nums, i - 2, target - p), 3, "+" + ppp);
-            addto(rs, collect(nums, i - 2, target + p), 3, "-" + ppp);
+                addto(rs, add, "+", info.str);
+                addto(rs, minus, "-", info.str);
+            }
         }
 
         return rs;
     }
 
-    private void addto(List<String> rs, List<String> collect, int i, String s) {
+    private void addto(List<String> rs, List<String> collect, String op, String s) {
         for(String c : collect){
-            String k = c;
-            rs.add(c + s);
+            rs.add(c + op + s);
         }
     }
 
+    private List<Info> construct(int start, int end, int[] nums, List<Info> cache) {
+        if(start == end){
+            Info info = new Info();
+            info.lastNum = nums[end];
+            info.str = nums[end] + "";
+            info.c = nums[end];
+
+            List<Info> s = new ArrayList<>();
+            s.add(info);
+
+            return s;
+        }
+
+        int c = nums[start];
+        List<Info> s = new ArrayList<>(cache.size() * 2 + 1);
+        for ( Info info : cache){
+            //use * and use connect
+            if(info.valid) {
+                s.add(info.newProduct(c));
+            }
+            Info i = info.toConnect(c);
+            if(i != null) {
+                s.add(i);
+            }
+        }
+
+        return s;
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println(new Solution().addOperators("232", 8));
+        System.out.println(new Solution().addOperators("1005", 5));
+        System.out.println(new Solution().addOperators("000", 0));
+        System.out.println(new Solution().addOperators("10000009", 9));
+        System.out.println(new Solution().addOperators("123456789", 45));
+        System.out.println(new Solution().addOperators("1000", 0));
+    }
+
+    public static class Info {
+        public int c;
+        public String str;
+        public int lastNum;
+        public boolean valid = true;
+
+        public Info newProduct(int c) {
+            Info info = new Info();
+            info.c = c * this.c;
+            info.str = c + "*" + this.str;
+            info.lastNum = c;
+            return info;
+        }
+
+        public Info toConnect(int c) {
+            this.str = c + this.str;
+            if(c == 0) {
+                this.valid = false;
+                return this;
+            }
+            this.valid = true;
+            String[] cs = str.split("\\*");
+            int p = 1;
+            try {
+                this.lastNum = Integer.parseInt(cs[0]);
+            } catch (Exception e) {
+                return null;
+            }
+            for (String k : cs) {
+                p = p * Integer.parseInt(k);
+            }
+            this.c = p;
+            return this;
+        }
     }
 }
